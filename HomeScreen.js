38f03b3b11d54839d42879cc920429d1145c450c
h-michaelson <71196124+h-michaelson@users.noreply.github.com>
2024-04-06 15:02:06 -0500

    first app

---
+++ HomeScreen.js
@@ -0,0 +1,21 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to React Native App!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
