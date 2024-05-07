0f75a3ef621dfb05bddfbf1857b4af307bde1330
h-michaelson <71196124+h-michaelson@users.noreply.github.com>
2024-05-06 22:12:59 -0500

    hi

--- shortBusterExpo/App.js
+++ shortBusterExpo/App.js
@@ -1,7 +1,7 @@
-// Import necessary modules
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Linking } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
 import SetupScreen from './SetupScreen'; // Import the SetupScreen component
 import OpenPage from './OpenPage';
@@ -11,12 +11,27 @@ const Stack = createStackNavigator();
 
 // Define the main App component
 export default function App() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Check if the app was opened from another app
+    Linking.getInitialURL().then(url => {
+      if (url) {
+        console.log(url);
+        // If opened from another app, navigate to OpenPage
+        navigation.navigate('OpenPage');
+      }
+    });
+  }, [navigation]); // Run this effect only once when the component mounts
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Setup" component={SetupScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="OpenPage" component={OpenPage} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
